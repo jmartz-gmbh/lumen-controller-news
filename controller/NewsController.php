254b5a3b0557b3d13a2d68aec7d208f6f3641de7
Jonathan Martz <git@jmartz.de>
2020-06-14 18:51:48 +0200

    remoe unused class

--- controller/NewsController.php
+++ controller/NewsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use \http\Env\Response;
 use \Illuminate\Http\Request;
 use \Illuminate\Support\Facades\DB;
-use Validator;
 
 class NewsController extends Controller
 {

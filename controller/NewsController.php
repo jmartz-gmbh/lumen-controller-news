04d6b3451b9bbd6c53d93adc9d9f668ffec188ab
Jonathan Martz <git@jmartz.de>
2021-06-25 22:27:31 +0200

    update controller

--- controller/NewsController.php
+++ controller/NewsController.php
@@ -27,9 +27,27 @@ class NewsController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function view(Request $request, int $id){
-        $news = DB::table('news')
-            ->where('id','=',$id);
+    public function id(Request $request, int $id){
+        $news = DB::table('news')->where('id','=',$id);
+
+        $count = $news->count();
+
+        if($count === 1){
+            $this->addData('news',$news->first());
+        }
+        else{
+            $this->addMessage('success','News doesnt exists.');
+        }
+
+        return $this->getResponse();
+    }
+
+    /**
+     * @param  Request  $request
+     * @return Response
+     */
+    public function key(Request $request, string $key){
+        $news = DB::table('news')->where('key','=',$key);
 
         $count = $news->count();
 
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/news/view/{id}', [
     'middleware' => [],
-    'uses' => 'App\Http\Controllers\NewsController@view'
+    'uses' => 'App\Http\Controllers\NewsController@id'
+]);
+
+Route::get('/news/{key}', [
+    'middleware' => [],
+    'uses' => 'App\Http\Controllers\NewsController@key'
 ]);
 
 Route::get('/news/all', [
